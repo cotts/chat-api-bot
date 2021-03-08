acb6ef8ce8b0a72bfc1f2d0a2703ba7fd41ef0ba
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-03-08 20:59:39 -0300

    [user] [l] : get user to log in

--- src/api/v1/user/user.service.js
+++ src/api/v1/user/user.service.js
@@ -8,3 +8,23 @@ import User from './user.model'
 async function userExists(username) {
   return User.findOne({ username }).then((data) => !!data)
 }
+
+/**
+ *  Retrieve user using login and password
+ * @param {String} username - user login name
+ * @param {String} password - user password
+ * @returns {Object}
+ */
+export async function getUser(username, password) {
+  //TBD - Password Enctrypt
+  try {
+    const user = await User.findOne({ username })
+    if (!user || user.password !== password) {
+      throw new Error('Username or password is incorrect')
+    }
+    delete user.password
+    return user
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
