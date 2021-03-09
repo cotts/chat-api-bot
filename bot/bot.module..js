0b0b1e36ebdb2ed34a1eed4792d711aa525d2b62
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-03-09 11:11:34 -0300

    [bot] [l] : create queue

--- bot/bot.module..js
+++ bot/bot.module..js
@@ -7,5 +7,25 @@ dotenv.config()
 const connectionString = process.env.RABBITMQ_SERVER
 const queue = process.env.RABBITMQ_QUEUE
 
+/**
+ * Connect to RABBITMQ Server
+ * @returns {RabbitMQ Channel}
+ */
 const connect = () =>
   amqp.connect(connectionString).then((conn) => conn.createChannel())
+
+/**
+ * Create RabbitMQ Queue
+ * @param {RabbitMQ Channel} channel
+ * @param {RabbitMQ Queue} queue
+ * @returns {Channel Queue}
+ */
+const createQueue = (channel, queue) =>
+  new Promise((resolve, reject) => {
+    try {
+      channel.assertQueue(queue, { durable: true })
+      resolve(channel)
+    } catch (err) {
+      reject(err)
+    }
+  })
