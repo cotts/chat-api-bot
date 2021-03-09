893e5f11ecd6dea451707e16d88a6bcf37e4baa9
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-03-08 21:01:12 -0300

    [user] [l] : create user controller function

--- src/api/v1/user/user.controller.js
+++ src/api/v1/user/user.controller.js
@@ -7,3 +7,11 @@ export function getUser(req, res) {
     .then((data) => res.json(data))
     .catch((error) => res.status(400).send(error.message))
 }
+
+export function create(req, res) {
+  const { name, username, password } = req.body
+  service
+    .createUser(name, username, password)
+    .then((data) => res.json(data))
+    .catch((error) => res.status(400).send(error.message))
+}
